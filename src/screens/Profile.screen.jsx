23d68b624ca16c24506433419431e09fbab2a1a0
Lawrence J <lawrenceg563@gmail.com>
2021-09-11 16:15:51 +0530

    Fetching posts created by logged in user and displays in profile screen also display current user name from redux state context api

--- src/screens/Profile.screen.jsx
+++ src/screens/Profile.screen.jsx
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import {UserContext} from '../App';
 
-const Profile = ()=>{
-    return(
-        <div style={{maxWidth:"550px",margin:"auto"}}>
-            <div style={{
-                display:"flex",
-                justifyContent:"space-around",
-                margin:"18px 0px",
-                borderBottom:"1px solid grey"
-            }}>
-                <div>
-                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}} src="https://cdn.sprintally.com/img/2018/05/kristina-pimenova4.jpg"/>
-                </div>
-                <div>
-                    <h4>Div Alisha</h4>
-                    <div style={{display:"flex",justifyContent:"space-between",width:"100%"}}>
-                        <h6>40 posts</h6>
-                        <h6>52 followers</h6>
-                        <h6>31 following</h6>
-                    </div>
-                </div>
-            </div>
+const Profile = () => {
+  const [mypic, setPics] = useState([]);
+  const {state} = useContext(UserContext);
 
-            <div className="gallery">
-                <img className="item" src="https://picsum.photos/200?random=1" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=2" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=3" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=4" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=5" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=6" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=7" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=8" alt='pic'/>
-                <img className="item" src="https://picsum.photos/200?random=9" alt='pic'/>
-                
-            </div>
+  useEffect(() => {
+    fetch("/mypost", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setPics(result.mypost);
+      });
+  }, []);
+  return (
+    <div style={{ maxWidth: "550px", margin: "auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px 0px",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div>
+          <img
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+            src="https://cdn.sprintally.com/img/2018/05/kristina-pimenova4.jpg"
+          />
         </div>
-    )
-}
+        <div>
+          <h4>{state.name}</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <h6>40 posts</h6>
+            <h6>52 followers</h6>
+            <h6>31 following</h6>
+          </div>
+        </div>
+      </div>
+
+      <div className="gallery">
+        {mypic.map((item) => (
+          <img key={item._id} className="item" src={item.photo} alt={item.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
