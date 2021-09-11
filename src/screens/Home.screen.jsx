6c28994c96b526df7ccf0f585d5f707a19d5866f
Lawrence J <lawrenceg563@gmail.com>
2021-09-12 01:00:54 +0530

    implemented comments feature post comment and display comments on post

--- src/screens/Home.screen.jsx
+++ src/screens/Home.screen.jsx
@@ -73,6 +73,34 @@ const Home = () => {
     
 }
 
+const makeComment = (text,postId)=>{
+  fetch("/comment",{
+    method:"put",
+    headers:{
+        "Content-Type":"application/json",
+        "Authorization":"Bearer "+localStorage.getItem("jwt")
+    },
+    body:JSON.stringify({
+        postId,
+        text
+    })
+})
+.then(res=>res.json())
+.then(result=>{
+    console.log("this is result",result)
+    const newData = data.map(item=>{
+        if(item._id==result._id){
+            return result
+        }
+        else{
+            return item
+        }
+    })
+    setData(newData)
+})
+.catch(err=>{console.log(err)})
+}
+
   return (
     <div className="home">
       {data.map((item) => {
@@ -92,7 +120,20 @@ const Home = () => {
               <h6>{item.likes.length} Likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
+              {
+                item.comments.map(record=>{
+                  return(
+                    <h6><span style={{fontWeight:"500"}}>{record.postedBy.name} </span>{record.text}</h6>
+                  )
+                })
+              }
+              <form onSubmit={(e)=>{
+                e.preventDefault();
+                makeComment(e.target[0].value,item._id);
+                e.target[0].value='';
+                }} >
               <input type="text" placeholder="add a comment" />
+              </form>
             </div>
           </div>
         );
