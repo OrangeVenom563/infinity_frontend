354a1de19af0fa99c21cc54448170cce58bd8f17
Lawrence J <lawrenceg563@gmail.com>
2021-09-11 13:52:20 +0530

    fetching posts from database and displaying on homepage

--- src/component/Navbar.component.jsx
+++ src/component/Navbar.component.jsx
@@ -8,6 +8,7 @@ const NavBar = () => {
   const RenderList = () => {
     if(state){
       return [
+        // <li><Link to="/">Home</Link></li>,
         <li><Link to="/profile">Profile</Link></li>,
         <li><Link to="/createpost">Create Post</Link></li>
       ]
--- src/screens/Home.screen.jsx
+++ src/screens/Home.screen.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Home = ()=>{
-    return(
-        <div className="home">
-            <div className="card home-card">
-                <h5>Div Alisha</h5>
-                <div className="card-image">
-                    <img src="https://picsum.photos/300/200" alt="post pic"/>
-                </div>
-                <div className="card-content">
-                <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>title</h6>
-                    <p>This is great</p>
-                    <input type='text' placeholder="add a comment"/>
-                </div>
+const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/allpost", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.posts);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {data.map((item) => {
+        return (
+          <div className="card home-card" key={item._id}>
+            <h5>{item.postedBy.name}</h5>
+            <div className="card-image">
+              <img src={item.photo} alt="post pic" />
             </div>
-        </div>
-    )
-}
+            <div className="card-content">
+              <i className="material-icons" style={{ color: "red" }}>
+                favorite
+              </i>
+              <h6>{item.title}</h6>
+              <p>{item.body}</p>
+              <input type="text" placeholder="add a comment" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Home;
