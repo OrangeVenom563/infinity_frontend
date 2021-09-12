b7908ca6c373e9e8fe024010d3b149e7158c7471
Lawrence J <lawrenceg563@gmail.com>
2021-09-12 12:09:09 +0530

    Shows delete icon on posted created by logged in user and implemented feature to delete post

--- src/screens/Home.screen.jsx
+++ src/screens/Home.screen.jsx
@@ -101,9 +101,28 @@ const makeComment = (text,postId)=>{
 .catch(err=>{console.log(err)})
 }
 
+const deletePost = (postid)=>{
+  fetch(`/deletepost/${postid}`,{
+    method:"delete",
+    headers:{
+      "Authorization":"Bearer "+localStorage.getItem("jwt")
+    }
+  })
+  .then(res => res.json())
+  .then(result=>{
+    console.log(result);
+    const newData = data.filter(item=>{
+      return item._id !== result._id;
+    })
+    setData(newData);
+  })
+  .catch(err=>console.log(err))
+}
+
   return (
     <div className="home">
       {data.map((item) => {
+        console.log({posted:item.postedBy._id,state:state._id,item:item._id});
         return (
           <div className="card home-card" key={item._id}>
             <h5>{item.postedBy.name}</h5>
@@ -111,11 +130,16 @@ const makeComment = (text,postId)=>{
               <img src={item.photo} alt="post pic" />
             </div>
             <div className="card-content">
+              
               <i className="material-icons" style={{ color: "red" }}>favorite</i>
-              {
+              { 
                   item.likes.includes(state._id)?
                   <i className="material-icons" onClick={()=>{unlikePost(item._id)}}>thumb_down</i>:
                   <i className="material-icons" onClick={()=>{likePost(item._id)}}>thumb_up</i>
+              }
+              {
+                item.postedBy._id == state._id &&
+                <i className="material-icons right" onClick={()=>{deletePost(item._id)}}>delete</i>
               }
               <h6>{item.likes.length} Likes</h6>
               <h6>{item.title}</h6>
@@ -123,7 +147,7 @@ const makeComment = (text,postId)=>{
               {
                 item.comments.map(record=>{
                   return(
-                    <h6><span style={{fontWeight:"500"}}>{record.postedBy.name} </span>{record.text}</h6>
+                    <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name} </span>{record.text}</h6>
                   )
                 })
               }
